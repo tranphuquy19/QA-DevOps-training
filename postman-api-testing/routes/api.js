cc9ac1a17a392c64d1f76068dd0d3e89e3fdfe92
tranphuquy19 <tranphuquy19@gmail.com>
2023-10-06 04:34:27 +0000

    add endpoints for PT

--- postman-api-testing/routes/api.js
+++ postman-api-testing/routes/api.js
@@ -6,4 +6,54 @@ router.get('/', function(req, res, next) {
   res.status(200).json({ message: 'Welcome to the Postman API Testing App' });
 });
 
+/* GET random success endpoint. */
+router.get('/success', function(req, res, next) {
+  var random = Math.floor(Math.random() * 2);
+  if (random === 0) {
+    var responseTime = Math.floor(Math.random() * 1000);
+    setTimeout(function() {
+      res.status(200).json({ message: 'This is a successful response' });
+    }, responseTime);
+  } else {
+    var responseTime = Math.floor(Math.random() * 1000);
+    setTimeout(function() {
+      res.status(500).json({ message: 'This is an error response' });
+    }, responseTime);
+  }
+});
+
+/* GET random failure endpoint. */
+router.get('/failure', function(req, res, next) {
+  var random = Math.floor(Math.random() * 2);
+  if (random === 0) {
+    var responseTime = Math.floor(Math.random() * 1000);
+    setTimeout(function() {
+      res.status(500).json({ message: 'This is an error response' });
+    }, responseTime);
+  } else {
+    var responseTime = Math.floor(Math.random() * 1000);
+    setTimeout(function() {
+      res.status(200).json({ message: 'This is a successful response' });
+    }, responseTime);
+  }
+});
+
+/* GET random delay endpoint. */
+router.get('/delay', function(req, res, next) {
+  var random = Math.floor(Math.random() * 2);
+  if (random === 0) {
+    var responseTime = Math.floor(Math.random() * 1000);
+    setTimeout(function() {
+      res.status(200).json({ message: 'This is a successful response' });
+    }, responseTime);
+  } else {
+    var responseTime = Math.floor(Math.random() * 1000);
+    setTimeout(function() {
+      res.status(500).json({ message: 'This is an error response' });
+    }, responseTime);
+  }
+});
+
+
+
 module.exports = router;
---
+++ readme.md
@@ -0,0 +1,5 @@
+# DevOps Principles and Practices for QA Professionals
+
+## Postman performance testing
+
+![img](imgs/postman-performance-testing.png)
