0fc42124643f0ab60e53a79600cec2c84d6497e0
tranphuquy19 <tranphuquy19@gmail.com>
2023-11-02 18:24:57 +0000

    remove test script

--- testkube/rancher/command.sh
+++
@@ -1,8 +0,0 @@
-helm repo add rancher-latest https://releases.rancher.com/server-charts/latest
-
-helm install rancher rancher-latest/rancher \
-   --namespace cattle-system \
-   --create-namespace \
-   --values=values.yaml
-
-# chay xong apply cai rancher.yaml de tao ingress nha ba
